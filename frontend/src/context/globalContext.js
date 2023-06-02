5370945c64c50cc1a418078fe0af754fc3a7f616
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-06-02 22:21:16 +0530

    Global Context

---
+++ frontend/src/context/globalContext.js
@@ -0,0 +1,123 @@
+import React, { useContext, useState } from "react"
+import axios from 'axios'
+
+
+const BASE_URL = "http://localhost:5000/api/v1/";
+
+
+const GlobalContext = React.createContext()
+
+export const GlobalProvider = ({children}) => {
+
+    const [incomes, setIncomes] = useState([])
+    const [expenses, setExpenses] = useState([])
+    const [error, setError] = useState(null)
+
+    // Incomes
+    // Copilot command - create add income function
+    const addIncome = async (income) => {
+        const response = await axios.post(`${BASE_URL}add-income`, income)
+            .catch((err) =>{
+                setError(err.response.data.message)
+            })
+        getIncomes()
+    }
+
+    // Copilot command - create function to get incomes
+    const getIncomes = async () => {
+        const response = await axios.get(`${BASE_URL}get-incomes`)
+        setIncomes(response.data)
+        console.log(response.data)
+    }
+
+    // Copilot command - create function to delete income
+    const deleteIncome = async (id) => {
+        const res  = await axios.delete(`${BASE_URL}delete-income/${id}`)
+        getIncomes()
+    }
+
+    // Copilot command - create function to get total income
+    const totalIncome = () => {
+        let totalIncome = 0;
+        incomes.forEach((income) =>{
+            totalIncome = totalIncome + income.amount
+        })
+
+        return totalIncome;
+    }
+
+    // Expenses
+
+    // Copilot command - create add expense function
+    const addExpense = async (income) => {
+        const response = await axios.post(`${BASE_URL}add-expense`, income)
+            .catch((err) =>{
+                setError(err.response.data.message)
+            })
+        getExpenses()
+    }
+
+    // Copilot command - create function to get expenses
+    const getExpenses = async () => {
+        const response = await axios.get(`${BASE_URL}get-expenses`)
+        setExpenses(response.data)
+        console.log(response.data)
+    }
+
+    // Copilot command - create function to delete expense
+    const deleteExpense = async (id) => {
+        const res  = await axios.delete(`${BASE_URL}delete-expense/${id}`)
+        getExpenses()
+    }
+
+    // Copilot command - create function to get total expenses
+    const totalExpenses = () => {
+        let totalIncome = 0;
+        expenses.forEach((income) =>{
+            totalIncome = totalIncome + income.amount
+        })
+
+        return totalIncome;
+    }
+
+    // Copilot command - create function to get total balance
+    const totalBalance = () => {
+        return totalIncome() - totalExpenses()
+    }
+
+    // Copilot command - create function to get transaction history
+    const transactionHistory = () => {
+        const history = [...incomes, ...expenses]
+        history.sort((a, b) => {
+            return new Date(b.createdAt) - new Date(a.createdAt)
+        })
+
+        return history.slice(0, 3)
+    }
+
+    return (
+        <GlobalContext.Provider value=
+        {{
+            addIncome,
+            getIncomes,
+            incomes,
+            deleteIncome,
+            expenses,
+            totalIncome,
+            addExpense,
+            getExpenses,
+            deleteExpense,
+            totalExpenses,
+            totalBalance,
+            transactionHistory,
+            error,
+            setError
+        }}>
+            {children}
+        </GlobalContext.Provider>
+    )
+}
+
+export const useGlobalContext = () =>{
+    return useContext(GlobalContext)
+}
